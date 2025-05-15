6689a1f71aeb278e24bf392f69aa79b39c131023
nodecspro <nowickow.artem2015@yandex.ru>
2025-05-15 21:01:43 +0300

    Refactor: Update settings and manage files for Django 5.2
    Update settings.py and manage.py to use double quotes for string literals, aligning with common Python style guides. Also, update .gitignore to specifically ignore the __pycache__ directory within the treemenu app.

--- myproject/treemenu/apps.py
+++ myproject/treemenu/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TreemenuConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'treemenu'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "treemenu"
